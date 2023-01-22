3f599896f5f7d66dbff5e4ba70ec5713fdea7a8f
aimansae <saeedaimann@gmail.com>
2023-01-22 12:01:30 +0000

    create post view

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,11 +2,28 @@ from rest_framework import serializers
 from .models import Post
 
 
-class PostSerializer(serializers.Serializer):
+class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.imageurl')
+
+    # field name in post model is image
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size larger thar 2MB!!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image with larger than 4096px'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px'
+            )
+            
+        return value    
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -17,5 +34,5 @@ class PostSerializer(serializers.Serializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image'
+            'title', 'content', 'image', 'image_filter'
         ]
