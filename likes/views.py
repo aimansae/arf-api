3b171bf9d2b256b634ed0eb8a8be0256e95ad6aa
aimansae <saeedaimann@gmail.com>
2023-01-25 17:08:09 +0000

    Likes

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Likes
+from .serializers import LikesSerializer
 # Create your views here.
+# https://www.django-rest-framework.org/api-guide/generic-views/#listcreateapiview
+
+
+class LikeList(generics.ListCreateAPIView):
+    # to make sure onlu auth users can like the posts
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikesSerializer
+    queryset = Likes.objects.all()
+
+    def form_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+# to retrieve and delete a like
+#https://www.django-rest-framework.org/api-guide/generic-views/#retrievedestroyapiview
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikesSerializer
+    queryset = Likes.objects.all()
+    
+    
